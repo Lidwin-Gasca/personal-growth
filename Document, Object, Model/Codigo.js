66151c4c97a800131c23bb7ade9abe0d07d0a65b
Lidwin-Gasca <67214075+Lidwin-Gasca@users.noreply.github.com>
2021-01-16 00:34:34 -0500

    Using DOM
    first time using Document, Object and Model

---
+++ Document, Object, Model/Codigo.js
@@ -0,0 +1,6 @@
+alert("si funciona");
+var ruta = window.location;
+document.write("estás en: " + ruta);
+console.log("este es un mensaje oculto para ti");
+console.log(ruta.origin);
+console.log(navigator);
