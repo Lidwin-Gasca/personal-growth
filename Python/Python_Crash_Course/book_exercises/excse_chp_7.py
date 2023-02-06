295cadf08fa9e7edefd7b6a027f9b1bee44e7f6b
Lidwin Gasca <memitogasca@gmail.com>
2023-02-05 23:19:53 -0500

    🐍Python: Ecercise

--- Python/Python_Crash_Course/book_exercises/excse_chp_7.py
+++ Python/Python_Crash_Course/book_exercises/excse_chp_7.py
@@ -47,3 +47,139 @@ if user % 10 == 0:
     print("\tSu numeor Si es multiplo de 10")
 else:
     print("\tSu numero No es multiplo de 10")
+
+
+
+
+
+
+
+
+
+
+
+
+    #7-4. Pizza Toppings:
+
+#Escriba un ciclo que solicite al usuario que ingrese una serie de ingredientes para la pizza hasta que ingrese 
+# un valor de 'salir'. 
+# A medida que ingresen cada ingrediente, imprima un mensaje que diga que agregará ese ingrediente a su pizza.
+
+coberturas = []
+prompt = "\nIngrese su vuberturas para su pizza."
+prompt += "\n(Escriba 'salir' para dejar de dar coberturas.)\n\t\t\t\t\t\t"
+while True:
+    cobertura = input(prompt)
+
+    if cobertura == 'salir':
+        print(f"Muy bien las coberturas que escogio son: {coberturas}")
+        break
+    else:
+        coberturas.append(cobertura)
+        print(f"Muy bien, {cobertura.title()} agregado.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    #7-5. Movie Tickets:
+
+#Un cine cobra diferentes precios de boletos dependiendo de la edad de la persona. 
+# Si una persona es menor de 3 años, la entrada es gratuita; si son entre 3 y 12, el boleto cuesta $10; 
+# y si son mayores de 12 años, el boleto cuesta $15. Escriba un ciclo en el que pregunte a los usuarios 
+# su edad y luego dígales el costo de su boleto de cine.
+
+
+prompt = "\n\nCual es su edad?.  "
+
+while True:
+    edad = int(input(prompt))
+    
+    if edad <= -1:
+        print("\nEsto no es broma, es en serio ( •̀ ω •́ )✧")
+    elif edad <= 3:
+        print(f"\nSu crio puede entrar gratis.")
+    elif edad <= 12:
+        print("\nEl pase del niño tiene un costo de $10 dolares.")
+    elif edad <= 115:
+        print("\nEl pase de entrada le será de 15 dolares.")
+    else:
+        print("\nNo te mames mery jain")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    #7-6. Three Exits:
+
+#Escriba diferentes versiones del Ejercicio 7-4 o del Ejercicio 7-5 que hagan cada uno de los 
+# siguientes al menos una vez:
+
+#   🕳 Use una prueba condicional en la instrucción while para detener el ciclo.
+
+#   🕳 Use una variable activa para controlar cuánto tiempo se ejecuta el ciclo.
+
+#   🕳 Use una declaración de interrupción para salir del ciclo cuando el usuario ingrese un valor de 'salir'.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    #7-7. Infinity:
+
+#Escribe un ciclo que nunca termine y ejecútalo. (Para finalizar el ciclo, presione CTRL-C o cierre la 
+# ventana que muestra la salida).
--- Python/Random_py_files/empty_file_for_testing.py
+++ Python/Random_py_files/empty_file_for_testing.py
@@ -1,23 +1,22 @@
 
+# import requests
+# import datetime
+# import time
+
+# def retrieve_news():
+#     url = "https://api.example.com/north-carolina-news"
+#     response = requests.get(url)
+#     news = response.json()
+
+#     print("Latest News from North Carolina:")
+#     for article in news["articles"]:
+#         print("\n" + article["title"])
+#         print(article["description"])
+
+# while True:
+#     current_time = datetime.datetime.now()
+#     if current_time.hour == 7 and current_time.minute == 5:
+#         retrieve_news()
+#     time.sleep(60)
 
 
-
-import requests
-import datetime
-import time
-
-def retrieve_news():
-    url = "https://api.example.com/north-carolina-news"
-    response = requests.get(url)
-    news = response.json()
-
-    print("Latest News from North Carolina:")
-    for article in news["articles"]:
-        print("\n" + article["title"])
-        print(article["description"])
-
-while True:
-    current_time = datetime.datetime.now()
-    if current_time.hour == 7 and current_time.minute == 5:
-        retrieve_news()
-    time.sleep(60)
