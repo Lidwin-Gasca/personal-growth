f3f672eab4b2455ed992b4f339699f294f4e8c95
Lidwin Gasca <memitogasca@gmail.com>
2023-02-14 00:01:32 -0500

    🐍Python: Exercise

--- Python/Random_py_files/empty_file_for_testing.py
+++ Python/Random_py_files/empty_file_for_testing.py
@@ -22,3 +22,39 @@
 
 
 
+albumes = []
+
+def make_album(name, artist=None, songs=None):
+    """Returning a album"""
+    album = {'name': name}
+    if artist:
+        album['artist'] = artist
+    if songs:
+        album['songs'] = songs
+    
+    albumes.append(album)
+    return album
+
+
+while True:
+    print("\n\t---Describa un album---")
+    print("(ingrese la letra 'q' en cualquier momento para cerrar el programa)\n")
+
+    n_album = input("El nombre del album: ")
+    if n_album == 'q'.lower():
+        break
+    a_album = input("El artista/banda del album: ")
+    if a_album == 'q'.lower():
+        make_album(n_album)
+        break
+    s_album = input("Cuantas pistas/canciones tiene el album: ")
+    if s_album == 'q'.lower():
+        make_album(n_album, a_album)
+        break
+    make_album(n_album, a_album, s_album)
+
+for album in albumes:
+    name = album['name']
+    artist = album.get('artist', 'Unknown Artist')
+    songs = album.get('songs', 'Unknown')
+    print(f"\nAlbum: {name.title()}\nBand/Artist: {artist.title()}\nNumber of Songs: {songs}")
