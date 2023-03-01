43e57d8bc869ecea3e77d043a4511e3b0aa7f24a
Lidwin Gasca <memitogasca@gmail.com>
2023-02-28 22:33:27 -0500

    🐍Python: Excercise

---
+++ Python/Python_Crash_Course/book_exercises/excse_chp_9.py
@@ -0,0 +1,57 @@
+
+
+
+
+	#9-1. Restaurante: 
+#Haz una clase llamada Restaurante. 
+#El método _init_() para Restaurante debe almacenar dos atributos: un nombre_restaurante y un tipo de cocina. 
+#Cree un método llamado describe_restaurant () que imprima estas dos piezas de información, 
+#y un método llamado open_restaurant () que imprima un mensaje que indique que el restaurante está abierto.
+
+#Cree una instancia llamada restaurante de su clase. 
+#Imprima los dos atributos individualmente y luego llame a ambos métodos. 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	#9-2. Tres Restaurantes: 
+#Comience con su clase del Ejercicio 9-1. 
+#Cree tres instancias diferentes de la clase y llame a describe_restaurant() para cada una instancia.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	#9-3. Usuarios: 
+#Haz una clase llamada Usuario. 
+#Crea dos atributos llamados first_name y apellido, y luego crea varios otros atributos que normalmente se 
+#almacenan en un perfil de usuario. Haz un método llamado describe_user() que imprima un resumen de la 
+#información del usuario. Cree otro método llamado greeting_user() que imprima un saludo personalizado al usuario. 
+#Cree varias instancias que representen a diferentes usuarios y llame a ambos métodos para cada usuario.
