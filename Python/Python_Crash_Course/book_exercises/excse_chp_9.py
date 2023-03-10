d487775def471ecfb6f23998f5c183d08414e870
Lidwin Gasca <memitogasca@gmail.com>
2023-03-09 19:47:24 -0500

    🐍Python: Excercise

--- Python/Python_Crash_Course/book_exercises/excse_chp_9.py
+++ Python/Python_Crash_Course/book_exercises/excse_chp_9.py
@@ -49,7 +49,29 @@ restaurante_1.abierto_restaurante()
 #Comience con su clase del Ejercicio 9-1. 
 #Cree tres instancias diferentes de la clase y llame a describe_restaurant() para cada una instancia.
 
+class Restaurante:
+    """Simple descripcion de una cocina."""
 
+    def __init__(self, nombre_restaurante, tipo_de_cocina):
+        self.nombre_restaurante = nombre_restaurante
+        self.tipo_de_cocina = tipo_de_cocina
+        
+    def descripcion_restaurante(self):
+        print(f"El restaurante {self.nombre_restaurante} se especializa en {self.tipo_de_cocina}.")
+
+    def abierto_restaurante(self):
+        print(f"El restaurante de nombre {self.nombre_restaurante} se encuentra abierto en este momento.")
+
+restaurante_1 = Restaurante('El pollo loco', 'pollo rostizado')
+restaurante_2 = Restaurante('El Barril', 'mariscos')
+restaurante_3 = Restaurante('stella', 'economica')
+
+restaurante_1.descripcion_restaurante()
+restaurante_1.abierto_restaurante()
+restaurante_2.descripcion_restaurante()
+restaurante_2.abierto_restaurante()
+restaurante_3.descripcion_restaurante()
+restaurante_3.abierto_restaurante()
 
 
 
@@ -71,7 +93,34 @@ restaurante_1.abierto_restaurante()
 
 	#9-3. Usuarios: 
 #Haz una clase llamada Usuario. 
-#Crea dos atributos llamados first_name y apellido, y luego crea varios otros atributos que normalmente se 
-#almacenan en un perfil de usuario. Haz un método llamado describe_user() que imprima un resumen de la 
-#información del usuario. Cree otro método llamado greeting_user() que imprima un saludo personalizado al usuario. 
+#Crea dos atributos llamados nombre y apellido, y luego crea varios otros atributos que normalmente se 
+#almacenan en un perfil de usuario. Haz un método llamado describir_usuario() que imprima un resumen de la 
+#información del usuario. Cree otro método llamado saludando_usuario() que imprima un saludo personalizado al usuario. 
 #Cree varias instancias que representen a diferentes usuarios y llame a ambos métodos para cada usuario.
+
+class Usuario:
+    def __init__(self, nombre, apellido):
+        self.nombre = nombre
+        self.apellido = apellido
+
+    def describir_usuario(self):
+        print(f"\nNombre: {self.nombre}\nApellido: {self.apellido}")
+    
+    def saludando_usuario(self):
+        print(f"\tHola {self.nombre}, bienvenido a Python.\n")
+
+
+user_1 = Usuario('Lidwin', 'Gasca')
+user_2 = Usuario('Aritzi', 'Hernandez')
+user_3 = Usuario('Abel', 'Garcia')
+
+
+
+user_1.describir_usuario()
+user_1.saludando_usuario()
+
+user_2.describir_usuario()
+user_2.saludando_usuario()
+
+user_3.describir_usuario()
+user_3.saludando_usuario()
